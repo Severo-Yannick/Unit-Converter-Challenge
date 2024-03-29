f20a6a539bd5414f26683c13787da1c854179109
Yannick SEVERO <ysevero@squad.fr>
2024-03-29 16:07:19 +0100

    Initial Commit

---
+++ UnitConverterChallenge/UnitConverterChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterChallengeApp.swift
+//  UnitConverterChallenge
+//
+//  Created by Yannick Severo on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
