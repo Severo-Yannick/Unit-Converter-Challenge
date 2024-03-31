118c1be06baff8e3f790cf9bc311887326c1f1e2
Yannick SEVERO <ysevero@squad.fr>
2024-03-31 16:25:50 +0200

    feat: temperature

--- UnitConverterChallenge/ContentView.swift
+++ UnitConverterChallenge/ContentView.swift
@@ -8,17 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum TemperatureUnit: String, CaseIterable {
+        case celsius = "celsius"
+        case fahrenheit = "fahrenheit"
+        case kelvin = "kelvin"
+    }
+    
+    @State private var inputTemperatureUnit = TemperatureUnit.celsius
+    @State private var outTemperatureUnit = TemperatureUnit.celsius
+    @State private var inputValue: Double = 0.0
+    @State private var outputValue: Double = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            Form {
+                Section(header: Text(inputTemperatureUnit.rawValue)) {
+                    TextField("Enter your \(inputTemperatureUnit.rawValue) temperature", text: Binding<String>(
+                        get: { String(self.inputValue) },
+                        set: { newValue in self.inputValue = Double(newValue) ?? self.inputValue}
+                    ))
+                    .keyboardType(.decimalPad)
+                    
+                    self.pickerTemperature(unit: $inputTemperatureUnit)
+                        .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text(outTemperatureUnit.rawValue)) {
+                    TextField("Enter your \(outTemperatureUnit.rawValue) temperature", text: Binding<String>(
+                        get: { String(self.outputValue) },
+                        set: { newValue in self.outputValue = Double(newValue) ?? self.outputValue }
+                    ))
+                    .keyboardType(.decimalPad)
+                    
+                    self.pickerTemperature(unit: $outTemperatureUnit)
+                }
+                
+                Button(action: {
+                    self.convertTemperature()
+                }) {
+                    Text("Convert")
+                }
+            }
+            .navigationTitle("Unit Converter")
         }
-        .padding()
+    }
+    
+    func pickerTemperature(unit: Binding<TemperatureUnit>) -> some View {
+        Picker(selection: unit, label: Text("Unit")) {
+            ForEach(TemperatureUnit.allCases, id: \.self) { unit in
+                Text(unit.rawValue).tag(unit)
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+    }
+    
+    
+    func convertTemperature() {
+        switch (inputTemperatureUnit, outTemperatureUnit) {
+        case (.celsius, .celsius):
+            outputValue = inputValue
+        case (.celsius, .fahrenheit):
+            outputValue = (inputValue * 9/5) + 32
+        case (.celsius, .kelvin):
+            outputValue = inputValue + 273.15
+        case (.fahrenheit, .celsius):
+            outputValue = (inputValue - 32) / 1.8
+        case (.fahrenheit, .fahrenheit):
+            outputValue = inputValue
+        case (.fahrenheit, .kelvin):
+            outputValue = (inputValue + 459.67) / 1.8
+        case (.kelvin, .celsius):
+            outputValue = inputValue - 273.15
+        case (.kelvin, .fahrenheit):
+            outputValue = (inputValue * 1.8) - 459.67
+        case (.kelvin, .kelvin):
+            outputValue = inputValue
+        }
     }
 }
 
-#Preview {
-    ContentView()
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+#endif
